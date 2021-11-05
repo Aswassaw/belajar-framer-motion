afd9d489ae63841298cdd6c4bbfcadea62318c64
Andry Pebrianto <andrypeb227@gmail.com>
2021-11-05 20:36:44 +0700

    Variants (part 2)

--- src/components/Order.jsx
+++ src/components/Order.jsx
@@ -11,10 +11,25 @@ const containerVariants = {
     x: "0px",
 
     transition: {
-      delay: 0.2,
       type: "spring",
       stiffness: 90,
+      damping: 8,
+      // Semakin besar mass maka akan semakin lambat, dan sebaliknya
+      mass: 0.5,
+      // Animasi children tidak akan dijalankan sebelum semua animasi di elemen berjalan
+      when: "beforeChildren",
+      // Mengurutkan kemunculan children sesuai waktu yang ditentukan
+      staggerChildren: .5,
     },
+  },
+};
+
+const childVariants = {
+  hidden: {
+    opacity: 0,
+  },
+  visible: {
+    opacity: 1,
   },
 };
 
@@ -27,10 +42,14 @@ const Order = ({ pizza }) => {
       animate='visible'
     >
       <h2>Thank you for your order :)</h2>
-      <p>You ordered a {pizza.base} pizza with:</p>
-      {pizza.toppings.map((topping) => (
-        <div key={topping}>{topping}</div>
-      ))}
+      <motion.p variants={childVariants}>
+        You ordered a {pizza.base} pizza with:
+      </motion.p>
+      <motion.div variants={childVariants}>
+        {pizza.toppings.map((topping) => (
+          <div key={topping}>{topping}</div>
+        ))}
+      </motion.div>
     </motion.div>
   );
 };
