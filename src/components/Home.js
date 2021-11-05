e07f1503935dbe602d878b6a4fa35bad3f104486
Andry Pebrianto <andrypeb227@gmail.com>
2021-11-05 10:13:04 +0700

    Initial Animation State

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,30 +6,14 @@ const Home = () => {
   return (
     <motion.div
       className='home container'
+      initial={{ opacity: 0 }}
       animate={{
-        marginTop: "200px",
-        opacity: "0.6",
-        rotateZ: 180,
+        opacity: 1,
       }}
     >
-      <motion.h2
-        animate={{
-          fontSize: "40px",
-          color: "#ff2994",
-          x: "-80px",
-          y: "-30px",
-        }}
-      >
-        Welcome to Pizza Joint
-      </motion.h2>
+      <h2>Welcome to Pizza Joint</h2>
       <Link to='/base'>
-        <motion.button
-          animate={{
-            scale: 1.5,
-          }}
-        >
-          Create Your Pizza
-        </motion.button>
+        <motion.button animate={{}}>Create Your Pizza</motion.button>
       </Link>
     </motion.div>
   );
