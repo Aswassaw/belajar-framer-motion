e0c45175e61b5ff750d3d4519f9f3ce69f1fd1ef
Andry Pebrianto <andrypeb227@gmail.com>
2021-11-06 08:36:52 +0700

    Repeating Animation

--- src/components/Base.jsx
+++ src/components/Base.jsx
@@ -28,6 +28,19 @@ const nextVariants = {
   },
 };
 
+const buttonVariants = {
+  hover: {
+    scale: 1.1,
+    textShadow: "0px 0px 8px rgb(255,255,255)",
+    boxShadow: "0px 0px 8px rgb(255,255,255)",
+    transition: {
+      // Mengulangi semua animasi sebanyak yang ditentukan
+      yoyo: Infinity,
+      duration: 0.3,
+    },
+  },
+};
+
 const Base = ({ addBase, pizza }) => {
   const bases = ["Classic", "Thin & Crispy", "Thick Crust"];
 
@@ -56,18 +69,9 @@ const Base = ({ addBase, pizza }) => {
       </ul>
 
       {pizza.base && (
-        <motion.div
-          className='next'
-          variants={nextVariants}
-        >
+        <motion.div className='next' variants={nextVariants}>
           <Link to='/toppings'>
-            <motion.button
-              whileHover={{
-                scale: 1.1,
-                textShadow: "0px 0px 8px rgb(255,255,255)",
-                boxShadow: "0px 0px 8px rgb(255,255,255)",
-              }}
-            >
+            <motion.button variants={buttonVariants} whileHover='hover'>
               Next
             </motion.button>
           </Link>
--- src/components/Toppings.jsx
+++ src/components/Toppings.jsx
@@ -19,6 +19,19 @@ const containerVariants = {
   },
 };
 
+const buttonVariants = {
+  hover: {
+    scale: 1.1,
+    textShadow: "0px 0px 8px rgb(255,255,255)",
+    boxShadow: "0px 0px 8px rgb(255,255,255)",
+    transition: {
+      // Mengulangi semua animasi sebanyak yang ditentukan
+      yoyo: Infinity,
+      duration: 0.3,
+    },
+  },
+};
+
 const Toppings = ({ addTopping, pizza }) => {
   let toppings = [
     "Mushrooms",
@@ -54,13 +67,7 @@ const Toppings = ({ addTopping, pizza }) => {
       </ul>
 
       <Link to='/order'>
-        <motion.button
-          whileHover={{
-            scale: 1.1,
-            textShadow: "0px 0px 8px rgb(255,255,255)",
-            boxShadow: "0px 0px 8px rgb(255,255,255)",
-          }}
-        >
+        <motion.button variants={buttonVariants} whileHover='hover'>
           Order
         </motion.button>
       </Link>
